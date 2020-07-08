ed8759cf5cff19d23554615bc6de126a16bf9c37
Niraj Shah <niraj3k@gmail.com>
2020-07-08 11:50:16 +0100

    Initial setup

---
+++ src/AzureFace/AzureFaceServiceProvider.php
@@ -0,0 +1,43 @@
+<?php namespace AzureFace;
+
+use Illuminate\Support\ServiceProvider;
+
+class AzureFaceServiceProvider extends ServiceProvider {
+
+  /**
+   * Bootstrap the application events.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+      $this->publishes([__DIR__ . '/config/config.php' => config_path('azure-face.php')], 'azure-face');
+  }
+
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+      $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'azure-face');
+
+      $this->app->singleton('niraj-shah.laravel-azure-face-api', function ($app) {
+          return new Client(
+            $app['config']->get('cliniko.api_key')
+          );
+      });
+  }
+
+  /**
+  * Get the services provided by the provider.
+  *
+  * @return array
+  */
+  public function provides()
+  {
+      return ['niraj-shah.laravel-azure-face-api'];
+  }
+
+}
